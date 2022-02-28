5f8d683047984aba1eb720add65d9601c71fcaf5
Yulim Kim <yulimkim@email.unc.edu>
2022-02-27 19:18:46 -0500

    finallygit add .

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import express from 'express'
 
 var args = minimist(process.argv.slice(2))
 
-//const express = require('express')
 const app = express()
 const port = args.port || process.env.PORT || 5000
 
